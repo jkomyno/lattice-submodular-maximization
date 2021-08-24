0df9226fd001da69e1ee7a3f580e4cede76b23b1
Alberto Schiabel <skiabo97@gmail.com>
2021-08-24 10:10:18 +0200

    Improved random_vector_with_norm: now the zero vector can no longer be returned

--- utils/random_vector_with_norm.py
+++ utils/random_vector_with_norm.py
@@ -19,7 +19,8 @@ def random_vector_with_norm(rng: np.random.Generator,
 
     for i in range(f.n):
         max_num = min(max_num, norm - cum_sum)
-        min_num = min(max_num, max(min_num, norm - max_num * (f.n - i)))
+        max_after = max_num * (f.n - i - 1)
+        min_num = min(max_num, max(min_num, norm - cum_sum - max_after))
         entry = rng.integers(low=min_num, high=max_num + 1, size=1)
         q[i] = entry
         cum_sum += entry
