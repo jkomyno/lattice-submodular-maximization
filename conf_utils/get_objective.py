22870f02d9a629558ebabd9673d6e69725ba2340
Alberto Schiabel <skiabo97@gmail.com>
2021-08-24 12:49:56 +0200

    updated get_objective

--- conf_utils/get_objective.py
+++ conf_utils/get_objective.py
@@ -1,3 +1,4 @@
+from typing import Iterator, Tuple, List
 import numpy as np
 from omegaconf import DictConfig
 from objective import Objective, DemoMonotone, DemoNonMonotone
@@ -9,29 +10,36 @@ OBJ_MAP = {
 }
 
 
-def load_demo_monotone(rng: np.random.Generator, params):
+def load_demo_monotone(rng: np.random.Generator, params) -> Iterator[Tuple[Objective, int]]:
     """
     Generate a random set-modular, monotone function
     :param rng: numpy random generator instance
     :param params: 'params.demo_monotone' dictionary entry in conf/config.yaml
     """
-    return DemoMonotone(rng, n=params.n)
+    nbr: List[Tuple[int, int, int]] = params.benchmark.nbr
+
+    for (n, b, r) in nbr:
+        yield (DemoMonotone(rng, n=n, b=b), r)
 
 
-def load_demo_non_monotone(rng: np.random.Generator, params):
+def load_demo_non_monotone(rng: np.random.Generator, params) -> Iterator[Tuple[Objective, int]]:
     """
     Generate a random set-modular, non_monotone function
     :param rng: numpy random generator instance
     :param params: 'params.demo_non_monotone' dictionary entry in conf/config.yaml
     """
-    return DemoNonMonotone(rng, n=params.n)
+    nbr: List[Tuple[int, int, int]] = params.benchmark.nbr
+    
+    for (n, b, r) in nbr:
+        yield (DemoNonMonotone(rng, n=n, b=b), r)
 
 
-def get_objective(rng: np.random.Generator, cfg: DictConfig) -> Objective:
+def get_objective(rng: np.random.Generator, cfg: DictConfig) -> Iterator[Tuple[Objective, int]]:
     """
     Return an instance of the selected set-submodular objective
     :param rng: numpy random generator instance
     :param cfg: Hydra configuration dictionary
     """
-    objective_name = cfg.selected.objective
-    return OBJ_MAP[objective_name](rng, cfg.objectives[objective_name])
+    objective_name = cfg.obj.objective
+    print(f'Loading f: {objective_name}\n')
+    return OBJ_MAP[objective_name](rng, cfg.obj)
