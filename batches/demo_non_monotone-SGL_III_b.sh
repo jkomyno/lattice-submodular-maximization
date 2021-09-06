5f106aad335a8349292ee706928cd95883cc874b
Alberto Schiabel <skiabo97@gmail.com>
2021-09-06 16:39:42 +0200

    chore: updated batches

--- batches/demo_non_monotone-SGL_III_b.sh
+++ batches/demo_non_monotone-SGL_III_b.sh
@@ -4,13 +4,13 @@
 #SBATCH --ntasks-per-node=1          # tasks per node
 #SBATCH --cpus-per-task=1            # cpus per task
 #SBATCH --mem=6GB                    # memory per node
-#SBATCH --time=24:00:00              # time limit
-#SBATCH --error=stderr.demo_non_monotone.SSG.txt            
-#SBATCH --output=stdout.demo_non_monotone.SSG.txt           
-#SBATCH --partition=cpulong          # partition name
+#SBATCH --time=4:00:00               # time limit
+#SBATCH --error=stderr.demo_non_monotone.SGL_III_b.txt            
+#SBATCH --output=stdout.demo_non_monotone.SGL_III_b.txt           
+#SBATCH --partition=amdfast          # partition name
 ml Python/3.8.6-GCCcore-10.2.0
 
 python -u main.py \
   -m runtime=cluster \
   obj=demo_non_monotone \
-  algo=SSG
+  algo=SGL-III-b
