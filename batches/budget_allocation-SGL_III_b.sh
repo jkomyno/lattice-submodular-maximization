8aa41aec6a10143bd5ed2804044d2afec8c433fd
Alberto Schiabel <skiabo97@gmail.com>
2021-09-12 09:49:01 +0200

    chore: improved SLURM batches

--- batches/budget_allocation-SGL_III_b.sh
+++ batches/budget_allocation-SGL_III_b.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=1            # cpus per task
 #SBATCH --mem=16GB                   # memory per node
 #SBATCH --time=4:00:00               # time limit
-#SBATCH --error=stderr.budget_allocation.SGL_III.txt            
-#SBATCH --output=stdout.budget_allocation.SGL_III.txt           
-#SBATCH --partition=cpufast          # partition name
+#SBATCH --error=stderr.budget_allocation.SGL_III_b.txt            
+#SBATCH --output=stdout.budget_allocation.SGL_III_b.txt           
+#SBATCH --partition=cpulong          # partition name
 ml Python/3.8.6-GCCcore-10.2.0
 
 python -u main.py \
   -m runtime=cluster \
   obj=budget_allocation \
-  algo=SGL-III
+  algo=SGL-III-b
