efeec02d66464572c92665ed7890694f4eb8b28d
Alberto Schiabel <skiabo97@gmail.com>
2021-10-29 12:24:07 +0200

    budget_allocation: added support for trimmed bipartite graph such that |V|=n, where n is given

--- python/benchmark/dataset_utils/wikilens_ratings.py
+++ python/benchmark/dataset_utils/wikilens_ratings.py
@@ -1,11 +1,12 @@
 import numpy as np
 import itertools
 import networkx as nx
-from typing import Tuple
+from nptyping import NDArray
+from typing import Callable, Tuple
 from .. import utils
 
 
-def import_wikilens_ratings(rng: np.random.Generator, basedir: str) -> Tuple[nx.Graph, int]:
+def import_wikilens_ratings(rng: np.random.Generator, basedir: str) -> Callable[[int], Tuple[nx.Graph, NDArray[int]]]:
     """
     Import graph for budget_allocation.
     :return: the bipartite graph G=(V,T;W).
@@ -13,11 +14,15 @@ def import_wikilens_ratings(rng: np.random.Generator, basedir: str) -> Tuple[nx.
     localpath = 'dataset/konect/wikilens-ratings/out.wikilens-ratings'
     filepath = f'{basedir}/{localpath}'
 
-    G = nx.Graph()
     edges = []
 
+    #########################
+    #  Read entire dataset  #
+    #########################
+
     with open(filepath, 'r') as f:
-        # skip first line
+        # skip the first two lines
+        f.readline()
         f.readline()
 
         while True:
@@ -25,46 +30,73 @@ def import_wikilens_ratings(rng: np.random.Generator, basedir: str) -> Tuple[nx.
             if not line:
                 break
 
-            u_raw, v_raw, _, _ = line.split()
+            u_raw, v_raw, w_raw, _ = line.split()
             u, v = map(int, (u_raw, v_raw))
+            w = float(w_raw)
 
             channel_id = u
             customer_id = v
+            weight = max(float(w), 0.5)
 
-            edges.append((channel_id, customer_id))
+            edges.append((channel_id, customer_id, weight))
 
-        edges = list(map(
-            utils.snd,
-            filter(
-                lambda ii: not (
-                    ii[0] % 3  == 0 or
-                    ii[0] % 5  == 0
-                ), enumerate(edges)
-            )
-        ))
-        edges = edges[:2000]
+    channel_ids = set()
+    customer_ids = set()
+    for channel_id, customer_id, _ in edges:
+        channel_ids.add(channel_id)
+        customer_ids.add(customer_id)
+
+    ###########################
+    #  generate entire graph  #
+    ###########################
+
+    channel_ids_to_node_tmp = dict(zip(sorted(channel_ids), itertools.count(start=0, step=1)))
+    customer_ids_to_node_tmp = dict(zip(sorted(customer_ids), itertools.count(start=len(channel_ids), step=1)))
+    edges = list(map(lambda edge: (channel_ids_to_node_tmp[edge[0]], customer_ids_to_node_tmp[edge[1]], edge[2]), edges))
+
+    G_tmp = nx.Graph()
+
+    # add bipartite nodes
+    G_tmp.add_nodes_from(channel_ids_to_node_tmp.values(), bipartite=0)   # V
+    G_tmp.add_nodes_from(customer_ids_to_node_tmp.values(), bipartite=1)  # T
+
+    for u, v, weight in edges:
+        # weights are divided by 5 (the maximum rating) to turn them into influence probabilities
+        G_tmp.add_edge(u, v, weight=weight / 5)
+
+    ##############################
+    #  trim the graph on demand  #
+    ##############################
+
+    def trim_graph(n: int, r: int) -> Tuple[nx.Graph, NDArray[int]]:
+        nonlocal G_tmp
+
+        # select the largest 20 out-degree channels, remove the customers without edges        
+        largest_channels = set(map(utils.fst, sorted(G_tmp.degree, key=utils.snd, reverse=True)[:n]))
+        edges = [(u, v, w) for u, v, w in set(G_tmp.edges.data('weight')) if u in largest_channels]
+        surviving_customers = set(map(utils.snd, edges))
+
+        channel_ids_to_node = dict(zip(sorted(largest_channels), itertools.count(start=0, step=1)))
+        customer_ids_to_node = dict(zip(sorted(surviving_customers), itertools.count(start=len(channel_ids_to_node), step=1)))
+        edges = list(map(lambda edge: (channel_ids_to_node[edge[0]], customer_ids_to_node[edge[1]], edge[2]), edges))
         
-        channel_ids = set()
-        customer_ids = set()
-        for channel_id, customer_id in edges:
-            channel_ids.add(channel_id)
-            customer_ids.add(customer_id)
-
-        print(f'n channel_ids: {len(channel_ids)}')     # 65
-        print(f'n customer_ids: {len(customer_ids)}')   # 1027 
-        print(f'n edges: {len(edges)}')                 # 2000
-
-        channel_ids_to_node = dict(zip(sorted(channel_ids), itertools.count(start=0, step=1)))
-        customer_ids_to_node = dict(zip(sorted(customer_ids), itertools.count(start=len(channel_ids), step=1)))
-        edges = list(map(lambda edge: (channel_ids_to_node[edge[0]], customer_ids_to_node[edge[1]]), edges))
+        G = nx.Graph()
 
         # add bipartite nodes
         G.add_nodes_from(channel_ids_to_node.values(), bipartite=0)   # V
         G.add_nodes_from(customer_ids_to_node.values(), bipartite=1)  # T
 
-        weights = rng.uniform(low=0.0, high=1.0, size=(len(edges), ))
-
-        for (u, v), weight in zip(edges, weights):
+        for u, v, weight in edges:
             G.add_edge(u, v, weight=weight)
 
-    return G
+        print(f'n channel_ids: {len(channel_ids_to_node)}')     # 
+        print(f'n customer_ids: {len(customer_ids_to_node)}')   # 
+        print(f'n edges: {len(edges)}')                         # 
+
+        B = rng.integers(low=1, high=20, endpoint=True, size=(n, ))
+        print(f'B: {B.tolist()}')
+        print(f'|B|_inf: {np.sum(B)}; r: {r}')
+
+        return G, B
+
+    return trim_graph
