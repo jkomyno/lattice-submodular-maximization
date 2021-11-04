a60750062824728cd5f2448ed3932cdd39220216
Alberto Schiabel <skiabo97@gmail.com>
2021-11-04 19:14:28 +0100

    chore: updated batches

--- batch.sh
+++ batch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# squeue --format="%.18i %.9P %.30j %.8T %.10M %.9l %.6D %R" --me
+
 # cpufast
 sbatch ./batches/budget_allocation-SGL.sh
 sbatch ./batches/demo_monotone-SGL.sh
@@ -10,8 +12,8 @@ sbatch ./batches/demo_monotone_skewed-SGL.sh
 sbatch ./batches/budget_allocation-base.sh
 sbatch ./batches/demo_monotone-base.sh
 sbatch ./batches/demo_monotone_skewed-base.sh
-sbatch ./batches/budget_allocation-Soma-II.sh
-sbatch ./batches/demo_monotone-Soma-II.sh
-sbatch ./batches/demo_monotone_skewed-Soma-II.sh
+# sbatch ./batches/budget_allocation-Soma-II.sh
+# sbatch ./batches/demo_monotone-Soma-II.sh
+# sbatch ./batches/demo_monotone_skewed-Soma-II.sh
 # sbatch ./batches/demo_non_monotone-base.sh
 
--- batches/demo_monotone_skewed-SGL.sh
+++ batches/demo_monotone_skewed-SGL.sh
@@ -5,10 +5,10 @@
 #SBATCH --ntasks-per-node=3          # tasks per node
 #SBATCH --cpus-per-task=1            # cpus per task
 #SBATCH --mem=32GB                   # memory per node
-#SBATCH --time=4:00:00               # time limit
+#SBATCH --time=24:00:00              # time limit
 #SBATCH --error=stderr.demo_monotone_skewed-SGL.txt            
 #SBATCH --output=stdout.demo_monotone_skewed-SGL.txt           
-#SBATCH --partition=cpufast          # partition name
+#SBATCH --partition=cpu              # partition name
 ml Python/3.8.6-GCCcore-10.2.0
 
 python -u -m python.benchmark  \
